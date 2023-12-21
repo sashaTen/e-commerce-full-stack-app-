a99644af6873695d38de33511a18cd660ceb24ed
TEN ALEXANDER <sasha1keshten@gmail.com>
2023-12-21 19:31:22 +0900

    added the   contact   page  and  template  and also   added    {%%}{%%}  that  type  of    inheritance

--- store/core/urls.py
+++ store/core/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
-from .views import   index
+from .views import   index ,    contact
 
 urlpatterns = [
     
     path('',index, name='index'),
+    path('contact/'  ,  contact, name=  'contact')
  
 ]
