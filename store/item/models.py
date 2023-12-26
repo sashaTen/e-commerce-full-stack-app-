7a71789f5c44a3cbfc533a777a46dda1b1e65ee0
TEN ALEXANDER <sasha1keshten@gmail.com>
2023-12-26 17:37:36 +0900

    now the   names  of  categories  are   shown  in   admin   panel

--- store/item/models.py
+++ store/item/models.py
@@ -6,6 +6,8 @@ class   Category(models.Model):
     name   =  models.CharField(max_length =  255)
     class  Meta  :
         verbose_name_plural =  'Categories'
+    def  __str__(self) :
+        return self.name
 
 class Item(models.Model):
     name = models.CharField(max_length=255)
