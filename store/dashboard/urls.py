46171dad62b0def6ca533585ef5bbe6210a985fa
TEN ALEXANDER <sasha1keshten@gmail.com>
2023-12-31 18:19:41 +0900

    added delete   function    and users   can    delete  their   items

--- store/dashboard/urls.py
+++ store/dashboard/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import  dashboard
+from .views import  dashboard , delete
 
 urlpatterns = [
     
    path('', dashboard, name='dashboard'),
+   path('<int:pk>/delete/', delete, name='delete'),
     
  
 ]
--- store/dashboard/views.py
+++ store/dashboard/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render  ,get_object_or_404  , redirect
 from item.models   import    Item
 from django.contrib.auth.decorators   import   login_required 
 # Create your views here.
@@ -7,3 +7,12 @@ def    dashboard(request):
     items   =    Item.objects.filter(created_by = request.user)
 
     return    render(request  ,    'dashboard.html' ,  {"items"  :   items})
+
+
+
+
+@login_required
+def   delete(request  ,   pk):
+    item  =  get_object_or_404(Item ,  pk=pk ,  created_by=  request.user)
+    item.delete()
+    return   redirect('/dashboard/')
