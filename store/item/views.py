53b810f10c2d8026cfa83316c9db05f2698110c6
TEN ALEXANDER <sasha1keshten@gmail.com>
2024-01-03 13:13:40 +0900

    now the   category  page   shows   also   items   of that   category

--- store/item/views.py
+++ store/item/views.py
@@ -29,7 +29,8 @@ def detail(request, pk):
 
 def   category(request ,    pk):
     category_object   =     get_object_or_404(Category ,    pk=pk)
-    return  render(request ,     'category_page.html' ,     {"category"  :   category_object})
+    items =     Item.objects.filter(category =    category_object)
+    return  render(request ,     'category_page.html' ,     {"category"  :   category_object   ,    "items" :   items})
 
 
 
